007adb529e913db38283b8772a9f24d6d9c4a91e
Chris Radler <100222249+chrisradler@users.noreply.github.com>
2022-06-04 11:52:19 -0400

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-# Register your models here.
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date_created', 'author')
-
-
-admin.site.register(Post, PostAdmin)
